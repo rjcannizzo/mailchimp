3052afa8d7664605b2bba535c784a5d764cd802f
rc <rc.info@protonmail.com>
2020-11-04 16:31:23 -0600

    completed code to build shopify sales table

--- create_shopify_user_table.py
+++ create_shopify_user_table.py
@@ -20,20 +20,28 @@ def count_email_occurrences(shopify_data_folder=HOME_DIR.joinpath('data/shopify'
         with open(file, encoding='utf-8') as f:
             reader = csv.DictReader(f)
             for row in reader:
-                counts.get(row['Email'], 0) + 1
+                if row['Email']:
+                    counts[row['Email']] = counts.get(row['Email'], 0) + 1
 
     for email in counts:
-        if counts['email'] > 1:
-            print(email)
+        if counts[email] > 1:
+            print(f"'{email}', {counts[email]}")
 
 
-def get_shopify_user_values(shopify_data_folder=HOME_DIR.joinpath('data/shopify')):
+def get_shopify_user_values(shopify_data_folder):
+    """
+    Generator that yields email, total spent and total orders from any csv files found in the 'shopify_data_folder'.
+    This data is used when inserting data into the 'sales' table.
+    :param shopify_data_folder: directory with 1 or more 'Customer' csv files exported from Shopify.
+    :yield: tuple
+    """
     files = (f for f in Path(shopify_data_folder).glob('*.csv'))
     for file in files:
         with open(file, encoding='utf-8') as f:
             reader = csv.DictReader(f)
             for row in reader:
-                yield row['Email'], row['Total Spent'], row['Total Orders']
+                if row['Email']:
+                    yield row['Email'], row['Total Spent'], row['Total Orders']
 
 
 def create_shopify_sales_table(database):
@@ -47,13 +55,12 @@ def create_shopify_sales_table(database):
     create_table_query = """CREATE TABLE "sales" (
     "email"	TEXT, "total_spent" REAL, "order_count" INTEGER, PRIMARY KEY("email"));"""
 
-    # db = Database(database)
-    # db.run_script(drop_table_query)
-    # db.create_table(create_table_query)
-    # values = get_shopify_user_values(shopify_data_folder=HOME_DIR.joinpath('data/shopify'))
-    # db.insert_many(query=insert_query, values=values)
-    # print(f"Added {db.get_total_changes()} rows to Shopify 'sales' table.")
-    count_email_occurrences()
+    db = Database(database)
+    db.run_script(drop_table_query)
+    db.create_table(create_table_query)
+    values = get_shopify_user_values(shopify_data_folder=HOME_DIR.joinpath('data/shopify'))
+    db.insert_many(query=insert_query, values=values)
+    print(f"Added {db.get_total_changes()} rows to Shopify 'sales' table.")
 
 
 def main():
