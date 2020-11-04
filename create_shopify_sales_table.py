ba304426384ffc7517f1ac91d4dc4822e5630215
rc <rc.info@protonmail.com>
2020-11-04 16:55:17 -0600

    updated function docs

--- create_shopify_sales_table.py
+++ create_shopify_sales_table.py
@@ -1,8 +1,13 @@
 """
-Create and populate Shopify 'sales' database
-
-
-
+create_shopify_sales_table()
+    - Create and populate the Shopify 'sales' table.
+    - this table has the following columns: 'email', 'total_spent', 'order_count'
+    - NOTE: We create this table everytime we need to run queries.
+get_shopify_sales_values()
+    - A generator that yields the data needed to populate the 'sales' table.
+    - To get the data, export all Customers from Shopify to the 'shopify_data_folder'.
+WARNINGS:
+    - Shopify customer exports may have empty string values for Email and this causes insertion errors.
 11-4-2020
 """
 import csv
@@ -12,27 +17,13 @@ from rc.db.sqlite_3.oop import Database
 HOME_DIR = Path(__file__).resolve().parent
 
 
-def count_email_occurrences(shopify_data_folder=HOME_DIR.joinpath('data/shopify')):
-    counts = dict()
-
-    files = (f for f in Path(shopify_data_folder).glob('*.csv'))
-    for file in files:
-        with open(file, encoding='utf-8') as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                if row['Email']:
-                    counts[row['Email']] = counts.get(row['Email'], 0) + 1
-
-    for email in counts:
-        if counts[email] > 1:
-            print(f"'{email}', {counts[email]}")
-
-
-def get_shopify_user_values(shopify_data_folder):
+def get_shopify_sales_values(shopify_data_folder):
     """
-    Generator that yields email, total spent and total orders from any csv files found in the 'shopify_data_folder'.
-    This data is used when inserting data into the 'sales' table.
-    :param shopify_data_folder: directory with 1 or more 'Customer' csv files exported from Shopify.
+    Generator that yields from all csv files found in the 'shopify_data_folder'.
+    data yielded: email, total spent and total orders
+    This is used when inserting data into the 'sales' table.
+    To get the raw csv data, export all Customers from Shopify to the 'shopify_data_folder'.
+    :param shopify_data_folder: directory with 1 or more csv files exported from Shopify ('Customer' export).
     :yield: tuple
     """
     files = (f for f in Path(shopify_data_folder).glob('*.csv'))
@@ -47,8 +38,8 @@ def get_shopify_user_values(shopify_data_folder):
 def create_shopify_sales_table(database):
     """
     This function is used to both create AND populate the shopify 'sales' table.
-    NOTE: We drop and recreate the table each time rather than updating values.
-    :return:
+    NOTE: We drop and recreate the table each time rather than updating its values.
+    :return: None
     """
     drop_table_query = """DROP TABLE IF EXISTS 'sales'; """
     insert_query = """INSERT INTO sales ('email', 'total_spent', 'order_count') VALUES (?,?,?);"""
@@ -58,7 +49,7 @@ def create_shopify_sales_table(database):
     db = Database(database)
     db.run_script(drop_table_query)
     db.create_table(create_table_query)
-    values = get_shopify_user_values(shopify_data_folder=HOME_DIR.joinpath('data/shopify'))
+    values = get_shopify_sales_values(shopify_data_folder=HOME_DIR.joinpath('data/shopify'))
     db.insert_many(query=insert_query, values=values)
     print(f"Added {db.get_total_changes()} rows to Shopify 'sales' table.")
 
