5b0e2af83e444175b2e529ead74b117c9fed22ae
rc <rc.info@protonmail.com>
2020-11-05 15:16:03 -0600

    changed access to mc secret key

--- mc_test.py
+++ mc_test.py
@@ -3,23 +3,26 @@
 """
 import json
 from pathlib import Path
+
 import mailchimp_marketing as MailchimpMarketing
 from mailchimp_marketing.api_client import ApiClientError
+
 HOME_DIR = Path(__file__).resolve().parent
 
+
 def get_config():
-    config_file = HOME_DIR.joinpath('data/config.json')
+    config_file = Path.home().joinpath('Documents/_rc/mc_config.json')
     with open(config_file) as f:
         return json.load(f)
 
+
 def main():
-
-
+    config = get_config()
     try:
         client = MailchimpMarketing.Client()
         client.set_config({
-            "api_key": "YOUR_API_KEY",
-            "server": "us9"
+            "api_key": config['key'],
+            "server": config['server']
         })
         response = client.ping.get()
         print(response)
@@ -27,6 +30,12 @@ def main():
         print(error)
 
 
+def save_environ():
+    import os
+    environ = {k: v for k, v in os.environ.item()}
+    with open('junk.json', 'w', encoding='utf-8') as f:
+        json.dump(environ, f)
+
+
 if __name__ == '__main__':
-    config = get_config()
-    print(config)
+    main()
