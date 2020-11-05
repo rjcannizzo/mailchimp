d79e7942126e6e17707948de9bd2d1d42de54010
rc <rc.info@protonmail.com>
2020-11-05 08:40:12 -0600

    refactored mailchimp insert to another module

--- build_mailchimp_user_table.py
+++ build_mailchimp_user_table.py
@@ -1,4 +1,5 @@
 """
+This should only be run ONCE
 Used to create the initial Mailchimp subscriber table
 
 explore this: https://mailchimp.com/developer/api/transactional/exports/export-activity-history/
@@ -6,10 +7,9 @@ explore this: https://mailchimp.com/developer/api/transactional/exports/export-a
 pip install git+https://github.com/mailchimp/mailchimp-marketing-python.git
 11-5-2020
 """
-import csv
+
 from pathlib import Path
 from rc.db.sqlite_3.oop import Database
-from rc.api.utils import get_md5_hash, get_date_from_timestamp
 
 HOME_DIR = Path(__file__).resolve().parent
 
@@ -21,52 +21,12 @@ def create_user_table(db):
     db.create_table(create_query)
 
 
-def read_csv(csv_file):
-    """
-    Yield objects from csv file.
-    :param csv_file
-    :yield: order dict from csv.DictReader()
-    """
-    with open(csv_file) as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            email = row['Email Address']
-            email_id = get_md5_hash(email)
-            status = 'subscribed'
-            rating = int(row['MEMBER_RATING'])
-            created = get_date_from_timestamp(row['OPTIN_TIME'])
-            updated = get_date_from_timestamp(row['LAST_CHANGED'])
-            yield email_id, email, status, rating, created, updated
-
-
-def insert_user_data(db, csv_file):
-    """
-    Insert user date from API into the 'user' table.
-    Notes:
-        This was created to use API output but batch processing is poorly documented
-        7-28-2020: I cannot get mailchimp API batch processing to work
-        Will need to use csv files until resolved
-    :param csv_file:
-    :param db: database to use
-    :return: none
-    """
-    query = """INSERT INTO user (email_id, email, status, rating, created, updated) VALUES(?,?,?,?,?,?);"""
-    db.insert_many(query, read_csv(csv_file))
-    print(f"Added {db.get_total_changes()} rows to Mailchimp 'user' table.")
-
-
-def build_users_table(database, csv_data):
-    db = Database(database)
+def build_users_table(_database):
+    db = Database(_database)
     db.run_script("""DROP TABLE IF EXISTS 'user';""")
     create_user_table(db)
-    insert_user_data(db, csv_data)
-
-
-def main():
-    database = HOME_DIR.joinpath('db/test.db')
-    csv_data = HOME_DIR.joinpath('data/subscribers/subscribed_segment_export.csv')
-    build_users_table(database, csv_data)
 
 
 if __name__ == '__main__':
-    main()
+    database = HOME_DIR.joinpath('db/test.db')
+    build_users_table(database)
--- insert_mailchimp_user_data.py
+++ insert_mailchimp_user_data.py
@@ -1,5 +1,6 @@
 """
-Insert data from a csv file into the Mailchimp user table.
+Insert data from a csv file into the Mailchimp 'user' table. Used for both updating and creating the table.
+Existing users are ignored when updating.
 11-5-2020
 """
 import csv
@@ -10,40 +11,53 @@ from rc.api.utils import get_md5_hash, get_date_from_timestamp
 HOME_DIR = Path(__file__).resolve().parent
 
 
-def read_csv(csv_file):
+def get_subscriber_email_set(db):
+    """
+    Return a set of Mailchimp subscribers from the 'user' table.
+    :param db: database OOP object
+    :return: set
+    """
+    email_query = """SELECT email FROM user WHERE status = 'subscribed';"""
+    fetchall_object = db.fetch(email_query)
+    return {item['email'] for item in fetchall_object}
+
+
+def read_csv(csv_file, subscriber_set):
     """
     Yield objects from csv file.
-    :param csv_file
+    :param subscriber_set: a set of subscribers retrieved from the Mailchimp 'user' table.
+    :param csv_file:
+    Note: subscriber_set will be empty the first time the 'user' table is created
     :yield: order dict from csv.DictReader()
     """
     with open(csv_file) as f:
         reader = csv.DictReader(f)
         for row in reader:
             email = row['Email Address']
-            email_id = get_md5_hash(email)
-            status = 'subscribed'
-            rating = int(row['MEMBER_RATING'])
-            created = get_date_from_timestamp(row['OPTIN_TIME'])
-            updated = get_date_from_timestamp(row['LAST_CHANGED'])
-            yield email_id, email, status, rating, created, updated
+            if email not in subscriber_set:
+                email_id = get_md5_hash(email)
+                status = 'subscribed'
+                rating = int(row['MEMBER_RATING'])
+                created = get_date_from_timestamp(row['OPTIN_TIME'])
+                updated = get_date_from_timestamp(row['LAST_CHANGED'])
+                yield email_id, email, status, rating, created, updated
 
 
-def insert_user_data(db, csv_file):
+def insert_user_data(db, csv_file, subscriber_set):
     """
-    Insert user date from API into the 'user' table.
-    Notes:
-        This was created to use API output but batch processing is poorly documented
-        7-28-2020: I cannot get mailchimp API batch processing to work
-        Will need to use csv files until resolved
-    :param csv_file:
-    :param db: database to use
+    Insert user data from a Mailchimp exported csv into the 'user' table.
+    :param csv_file: path to a csv file with Mailchimp subscriber data.
+    :param subscriber_set: a set of subscribers retrieved from the Mailchimp 'user' table. Passed to read_csv().
+    :param db: database OOP object
     :return: none
     """
     query = """INSERT INTO user (email_id, email, status, rating, created, updated) VALUES(?,?,?,?,?,?);"""
-    db.insert_many(query, read_csv(csv_file))
+    db.insert_many(query, read_csv(csv_file, subscriber_set))
+    print(f"Added {db.get_total_changes()} rows to Mailchimp 'user' table.")
 
 
 if __name__ == '__main__':
     database = oop.Database(HOME_DIR.joinpath('db/test.db'))
     csv_data = HOME_DIR.joinpath('data/subscribers/subscribed_segment_export.csv')
-    insert_user_data(database, csv_data)
+    email_set = get_subscriber_email_set(database)
+    insert_user_data(database, csv_data, email_set)
